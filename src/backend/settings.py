64c8a6254aa953a9959b30726acbad6fd98dd1ae
Adam Parr <adamparr.uk@gmail.com>
2024-07-28 19:02:03 +0100

    Auth: implement TokenAuthentication with token and logout views

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework.authtoken",
     "backend.data",
+    "backend.interfaces.auth_token",
     "tests",
 ]
 
@@ -129,3 +131,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "data.CustomUser"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
--- src/backend/urls.py
+++ src/backend/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/auth_token/", include("backend.interfaces.auth_token.urls")),
 ]
