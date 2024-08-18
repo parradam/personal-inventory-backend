af51a0cf0dd585ad54f96a1ef2b44c62cbad1829
Adam Parr <adamparr.uk@gmail.com>
2024-08-18 01:52:53 +0100

    Interface layer: write API for deletion of an Item, test

--- src/backend/interfaces/item_management/urls.py
+++ src/backend/interfaces/item_management/urls.py
@@ -3,3 +3,4 @@ from django.urls import path
 from backend.interfaces.item_management import views
 
 urlpatterns = [path("items/", views.ItemList.as_view())]
+urlpatterns += [path("items/<int:pk>", views.ItemDetail.as_view())]
