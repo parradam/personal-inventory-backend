6e02ecce0723fef58fd78ca894bbeeb639f33afd
Adam Parr <adamparr.uk@gmail.com>
2024-10-02 22:59:13 +0100

    Data layer: create ItemEvent model, migrate

--- src/backend/data/models/__init__.py
+++ src/backend/data/models/__init__.py
@@ -1,4 +1,5 @@
 from backend.data.models.custom_user import CustomUser
 from backend.data.models.item import Item
+from backend.data.models.item_event import ItemEvent
 
-__all__ = ["CustomUser", "Item"]
+__all__ = ["CustomUser", "Item", "ItemEvent"]
